197300a7a17e122ec27f9d33fa34d511617eba01
Vishal <74290663+dr-cyberx@users.noreply.github.com>
2022-10-10 07:57:30 +0530

    understanding routing in express

--- server.js
+++ server.js
@@ -1,12 +1,47 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    console.log(req.params)
-    res.send('Hello world')
-})
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/app-data/data.json`));
+
+app.get("/api/ishealthy", (req, res) => {
+  console.log(req.params);
+  res.status(200).json({ message: "Hello world", status: 200 });
+});
+
+app.get("/api/tours", (req, res) => {
+  res.status(200).json({ result: tours.length, data: tours, status: 200 });
+});
+
+app.get("/api/tours/:id/:x/:y?", (req, res) => {
+  console.log(req.params);
+  res.status(200).json({
+    result: tours.filter((item) => item.id == req.params.id).length,
+    data: tours.filter((item) => item.id == req.params.id),
+    status: 200,
+  });
+});
+
+app.post("/api/tours", (req, res) => {
+  const id = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/app-data/data.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
+});
 
 app.listen(4000, () => {
-    console.log('The server is up at http://localhost:4000');
-})
+  console.log("The server is up at http://localhost:4000");
+});
